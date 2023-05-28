839406d39c14238e3eca1353269e0b88aa33c4fb
aaaaaaaalesha <sks2311211@mail.ru>
2023-05-28 13:44:32 +0300

    (api): endpoints for Tags added

---
+++ backend/foodgram/api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework.serializers import ModelSerializer
+
+from recipes.models import (
+    Tag,
+)
+
+
+class TagSerializer(ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import (
+    mixins,
+    viewsets,
+)
 
-# Create your views here.
+from recipes.models import (
+    Tag,
+)
+
+from .permissions import (
+    IsAdminOrReadOnly,
+    IsAuthorOrReadOnly,
+)
+
+from .serializers import (
+    TagSerializer,
+)
+
+
+class TagViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = (IsAdminOrReadOnly,)
