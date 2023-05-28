e26b86fe372f16b17466a17b017a3b77070432ec
aaaaaaaalesha <sks2311211@mail.ru>
2023-05-28 15:14:42 +0300

    (api): endpoints for Ingredients added

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -3,13 +3,23 @@ from rest_framework.routers import DefaultRouter
 
 from .views import (
     TagViewSet,
+    IngredientViewSet,
 )
 
 app_name = 'api'
 
 router = DefaultRouter()
 
-router.register('tags', TagViewSet)
+router.register(
+    r'tags',
+    TagViewSet,
+    basename='tags',
+)
+router.register(
+    r'ingredients',
+    IngredientViewSet,
+    basename='ingredients',
+)
 
 urlpatterns = [
     path('', include(router.urls)),
